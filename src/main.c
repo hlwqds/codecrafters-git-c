dfd013ec7177d91e1c888c618a1584336e72ee03
hlwqds <hl-ljj@outlook.com>
2026-05-20 22:09:46 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,8 +1,35 @@
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <zlib.h>
+
+char *decompress_zlib(char *in, size_t in_len, size_t *out_len) {
+    z_stream strm = {0};
+    inflateInit(&strm);
+    strm.avail_in = in_len;
+    strm.next_in = in;
+    size_t cap = in_len * 4;
+    char *out = malloc(cap);
+    strm.avail_out = cap;
+    strm.next_out = out;
+    inflate(&strm, Z_FINISH);
+    *out_len = strm.total_out;
+    inflateEnd(&strm);     
+    return out;
+} 
+
+static void parse_blob(char *blob, size_t len) {
+    size_t decom_len = 0;
+    blob = decompress_zlib(blob, len, &decom_len);
+    int content_len = atoi(blob + 5);
+    int header_len = strlen(blob);
+    char *content = blob + header_len + 1;
+    printf("%.*s", content_len, content);
+    free(blob);
+}
 
 int main(int argc, char *argv[]) {
     // Disable output buffering
@@ -38,6 +65,36 @@ int main(int argc, char *argv[]) {
         fclose(headFile);
         
         printf("Initialized git directory\n");
+    } else if (strcmp(command, "cat-file") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "Usage: ./your_program.sh <command> [<args>]\n");
+            return 1;
+        }
+        const char *flag = argv[2];
+        const char *hash = argv[3];
+        if (strcmp(flag, "-p") != 0) {
+            fprintf(stderr, "unsupportted flag\n");
+            return 1;
+        }
+        if (strlen(hash) != 40) {
+            fprintf(stderr, "invalid hash len\n");
+            return 1;
+        }
+        char path[PATH_MAX] = {0};
+        sprintf(path, ".git/objects/%.*s/%s", 2, hash, hash + 2);
+        FILE *fp = fopen(path, "rb");
+        if (fp == NULL) {
+            fprintf(stderr, "invalid hash len\n");
+            return 1;
+        }
+        fseek(fp, 0, SEEK_END);
+        size_t len = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        char *buf = malloc(len);
+        size_t size = fread(buf, 1, len, fp);
+        parse_blob(buf, size);
+        free(buf);
+        fclose(fp);
     } else {
         fprintf(stderr, "Unknown command %s\n", command);
         return 1;
